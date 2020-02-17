a8ace6bfbe5c13b39270166bf7f09a84af26a9f3
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-17 19:24:52 +0530

    Databse Created > > models.py added > performed init command > performed migration command > performed upgrade command > postgress created > Static HTML structure created > Config.py created > .env Corrected

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
 from app import app
 
+# @app.route('/', methods = ['POST', 'GET', 'DELETE'])
+# def http_method():
+#     if request.method == 'POST':
+#         return request.json.get('name')
+#
+#     if request.method == 'GET':
+#         return 'success getting response'
+#
+#     if request.method == 'DELETE':
+#         return 'success deletion'
+
 @app.route('/')
 def hello():
-    return 'Kshitij Bhatnagar'
+    return '<h1><center><b>Thank you</b></center></h1>' \
+           '<p>Hi</p>' \
+           '<p>Thank you for being part of our family. Hope you will have a <br>nice experience with us.</p>' \
+           '<p>For any queries visit our website.</p>' \
+           'Thank you<br>HU2k20.com'
