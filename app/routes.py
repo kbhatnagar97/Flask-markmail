5f0ddefdd52f7a595bd252e683ae458c0f9df7f1
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-18 15:21:46 +0530

    JSON-CURD operation working
    PUT
    > > GET
    > > POST
    > > DELETE
    working and update on postgress visible, with the use of request.json.get()

--- app/routes.py
+++ app/routes.py
@@ -3,24 +3,36 @@ from app import app, db
 from app.models import User
     # Student, Adress, ContactInfo, Page, Tag, Person
 
-@app.route('/profile/<username>/<email>', methods=['PUT','POST','GET','DELETE'])
-def http_method(username,email):
+@app.route('/user', methods=['PUT','POST','GET','DELETE'])
+
+def http_method():
+    username = request.json.get('username')
+    email = request.json.get('email')
     if request.method == 'PUT':
-        new_email = 'my_new_email@example.com'
+        username = request.json.get('username')
+        new_email = request.json.get('email')
         admin = User.query.filter_by(username=username).first()
         admin.email = new_email
         db.session.commit()
         return 'updated email id is '+ new_email
 
     elif request.method == 'GET':
-        user = User.query.filter_by(username=username,email=email)
-        user = user.first()
-        return 'email is {}'.format(email)
+        s={}
+        user = User.query.all()
+        print(user)
+        i=0
+        for use in user:
+            s[i]={
+                username:use.username,
+                email:use.email
+            }
+            i+=1
+        return s
 
     elif request.method == 'DELETE':
         User.query.filter_by(username=username,email=email).delete()
         db.session.commit()
-        return 'success deletion with username{} '.format(username)
+        return 'success deletion with username{}'.format(username)
 
     elif request.method == 'POST':
         user= User(username=username,email=email)
@@ -33,8 +45,7 @@ def http_method(username,email):
 
 
 @app.route('/')
-def template():
-    db.session.commit()
+def hello():
     return '<h1><center><b>Thank you</b></center></h1>' \
            '<p>Hi</p>' \
            '<p>Thank you for being part of our family. Hope you will have a <br>nice experience with us.</p>' \
