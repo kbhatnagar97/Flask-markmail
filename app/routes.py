15195a091acdaaeb9e5c02632a04f4118be85e9a
Kshitij Bhatnagar <kshitij.bhatnagar@hashedin.com>
2020-02-19 18:56:46 +0530

    Comments added
    a few tweaks made to the code apart from adding comments.

--- app/routes.py
+++ app/routes.py
@@ -8,23 +8,23 @@ from sqlalchemy.exc import IntegrityError
 import csv
     # Student, Adress, ContactInfo, Page, Tag, Person
 
-app.config['MAIL_SERVER']='localhost'
-app.config['MAIL_PORT'] = 2525
+app.config['MAIL_SERVER']='localhost' #running on this
+app.config['MAIL_PORT'] = 2525 #port number
 mail = Mail(app)
 
 class customException(Exception):
-    status_code = 400
+    status_code = 400 #Bad Request response status code indicates that the server cannot or will not process the request due to something that is perceived to be a client error
 
 @app.errorhandler(customException)
 def handel_error(error):
-    return 'CUSTOM EXCEPTION HANDLED'
+    return 'CUSTOM EXCEPTION HANDLED' #Exception handling message
 
 @app.route('/user', methods=['PUT','POST','GET','DELETE'])
 def customer():
-    csv_file = request.files['files']
-    csv_file = TextIOWrapper(csv_file, encoding='utf-8')
-    csv_dict = csv.DictReader(csv_file)
-    for row in csv_dict:
+    csv_file = request.files['files'] #gets input in bytes
+    csv_file = TextIOWrapper(csv_file, encoding='utf-8') #converts to string
+    csv_dict = csv.DictReader(csv_file) #converts bytes into ordered dictionary
+    for row in csv_dict: #row is the iterrator
         first_name = row['first_name']
         last_name = row['last_name']
         gender = row['gender']
@@ -35,26 +35,27 @@ def customer():
         zipcode = row['zipcode']
         phoneNumber = row['phoneNumber']
         registerationDate = row['registerationDate']
+        #put method
         if request.method == 'PUT':
             admin = User.query.filter_by(first_name=first_name,last_name=last_name,gender=gender,email=email,age=age,address=address,state=state,zipcode=zipcode,phoneNumber=phoneNumber,registerationDate=registerationDate).first()
-            admin.email = email
-            admin.first_name = first_name
-            admin.last_name = last_name
-            admin.gender = gender
-            admin.age = age
-            admin.address = address
-            admin.state = state
-            admin.zipcode = zipcode
-            admin.phoneNumber = phoneNumber
-            admin.registrationDate = registerationDate
+            admin.email = email #Assigning new email to variable admin
+            admin.first_name = first_name #Assigning new first_name to variable admin
+            admin.last_name = last_name #Assigning new second_name to variable admin
+            admin.gender = gender #Assigning new gender to variable admin
+            admin.age = age #Assigning new age to variable admin
+            admin.address = address #Assigning new address to variable admin
+            admin.state = state #Assigning new state to variable admin
+            admin.zipcode = zipcode #Assigning new zipcode to variable admin
+            admin.phoneNumber = phoneNumber #Assigning new phonenumber to variable admin
+            admin.registrationDate = registerationDate #Assigning new registerationDate to variable admin
             db.session.commit()
-            return 'updated email id is '+ email
-
+            return 'updated all fields'
+        #get Method
         elif request.method == 'GET':
             s={}
             user = User.query.all()
-            i=0
-            for use in user:
+            i=0 #defining iterating variable
+            for use in user: #iterating through all the atttributes of customer
                 s[i]={
                     "first_name" : use.first_name,
                     "last_name" : use.last_name,
@@ -69,12 +70,12 @@ def customer():
                 }
                 i+=1
                 return s
-
+        #Delete Method
         elif request.method == 'DELETE':
-            User.query.filter_by(email=email).delete()
+            User.query.filter_by(first_name=first_name,last_name=last_name,gender=gender,email=email,age=age,address=address,state=state,zipcode=zipcode,phoneNumber=phoneNumber,registerationDate=registerationDate).delete()
             db.session.commit()
-            return 'success deletion with username'
-
+            return 'success deletion'
+        #Post Method
         elif request.method == 'POST':
             user= User(first_name=first_name,last_name=last_name,gender=gender,email=email,age=age,address=address,state=state,zipcode=zipcode,phoneNumber=phoneNumber,registerationDate=registerationDate)
             try:
@@ -82,7 +83,7 @@ def customer():
                 db.session.commit()
             except IntegrityError:
                 raise customException
-        return 'Created new user with email {}'.format(email)
+        return 'Created new user'
 
 
 
@@ -105,19 +106,19 @@ def template():
         admin.para3 = new_para3
         db.session.commit()
         return 'updated template'
-
+    #Get Method
     elif request.method == 'GET':
         return "<h1><center>{}</center></h1> \
                <p>{}</p> \
                <p>{}</p> \
                <p>{}</p>".format(title , para1 , para2, para3)
-
+    #Delete Method
     elif request.method == 'DELETE':
         template=Template.query.filter_by(Title=title, para1=para1, para2=para2, para3=para3)
         db.session.delete(template.first())
         db.session.commit()
         return 'successful deletion of template'
-
+    #Post Method
     elif request.method == 'POST':
         template = Template(Title=title, para1=para1, para2=para2, para3=para3)
         db.session.add(template)
@@ -132,9 +133,3 @@ def send_mail():
    mail.send(msg)
    return "Email Sent"
 
-# def testfunction1():
-#     with open('Customer.csv', 'r') as file:
-#         csv_file = csv.DictReader(file)
-#         for row in csv_file:
-#             print(row)
-#             return row
